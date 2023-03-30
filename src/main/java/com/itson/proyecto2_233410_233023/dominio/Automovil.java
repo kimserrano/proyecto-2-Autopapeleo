e431fa0f9afd68fef3f3b0fda39581a0ff948ea2
Kim Serrano Uribe <alejandra.ser.ur@gmail.com>
2023-03-30 10:22:01 -0700

    Se agregó la herencia para vehiculo agregando la entidad Automovil, ya documentada

---
+++ src/main/java/com/itson/proyecto2_233410_233023/dominio/Automovil.java
@@ -0,0 +1,64 @@
+/*
+Clase Automovil.java creada el 29/03/2023.
+ */
+package com.itson.proyecto2_233410_233023.dominio;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+/**
+ * Clase que representa a un automovil que le pertenece a la persona
+ *
+ * @author Gabriel x Kim
+ */
+@Table(name = "Automoviles")
+@DiscriminatorValue(value = "Automovil")
+@Entity
+public class Automovil extends Vehiculo implements Serializable {
+
+    /**
+     * Constructor de defecto para construir objetos Automovil
+     */
+    public Automovil() {
+    }
+
+    /**
+     * Constructor para construir objetos Automovil
+     *
+     * @param numeroSerie Representa el numero de serie del vehiculo
+     * @param modelo Representa el modelo del vehiculo
+     * @param marca Representa la marca del vehiculo
+     * @param linea Representa la linea del vehiculo
+     * @param color Representa el color del vehiculo
+     * @param persona Representa a la persona duenia del vehiculo
+     */
+    public Automovil(String numeroSerie, String modelo, String marca, String linea, String color, Persona persona) {
+        super(numeroSerie, modelo, marca, linea, color, persona);
+    }
+
+    /**
+     * Constructor para construir objetos Automovil
+     *
+     * @param numeroSerie Representa el numero de serie del vehiculo
+     * @param modelo Representa el modelo del vehiculo
+     * @param marca Representa la marca del vehiculo
+     * @param linea Representa la linea del vehiculo
+     * @param color Representa el color del vehiculo
+     * @param persona Representa a la persona duenia del vehiculo
+     * @param placas Representa la lista de placas que tiene o ha tenido el
+     * vehiculo
+     */
+    public Automovil(String numeroSerie, String modelo, String marca, String linea, String color, Persona persona, List<Placa> placas) {
+        super(numeroSerie, modelo, marca, linea, color, persona, placas);
+    }
+
+}
--- src/main/java/com/itson/proyecto2_233410_233023/dominio/Vehiculo.java
+++ src/main/java/com/itson/proyecto2_233410_233023/dominio/Vehiculo.java
@@ -6,10 +6,13 @@ package com.itson.proyecto2_233410_233023.dominio;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -21,6 +24,8 @@ import javax.persistence.Table;
  * @author Gabriel x Kim
  */
 @Entity
+@DiscriminatorColumn(name="tipo")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Table(name = "Vehiculos")
 public class Vehiculo implements Serializable {
 
